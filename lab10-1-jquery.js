302fa12f7f680b1f5e2b7da3d39d20521209a236
Sonnto <90372802+Sonnto@users.noreply.github.com>
2022-11-28 11:32:01 -0500

    complete lab

--- lab10-1-jquery.js
+++ lab10-1-jquery.js
@@ -1,2 +1,22 @@
 //LAB 10 - 1 FAQ PAGE
+jQuery(window).on("load", function () {
+  //hides the p
+  $("p").hide();
 
+  //toggle show-hide when clicking the title
+
+  $("h2").on("click", function () {
+    $(this).next("p").slideToggle(3000);
+  });
+
+  //mouseover and mouseleave change or hover
+
+  $("p").hover(
+    function () {
+      $(this).addClass("textHovered");
+    },
+    function () {
+      $(this).removeClass("textHovered");
+    }
+  );
+});
--- lab10-2-jquery.js
+++ lab10-2-jquery.js
@@ -1,2 +1,22 @@
 //LAB 10 - 2 INVENTORY PAGE
+jQuery(window).on("load", function () {
+  //hide text on load
+  $(".desc").hide();
 
+  //row red on mouseenter and mouseleave
+
+  $("tr").hover(
+    function () {
+      $(this).css("background", "red");
+    },
+    function () {
+      $(this).css("background", "white");
+    }
+  );
+
+  //toggle show hide the specific row
+  $("tr").on("click", function () {
+    $(".desc").hide();
+    $(this).find("td.desc").toggle();
+  });
+});
